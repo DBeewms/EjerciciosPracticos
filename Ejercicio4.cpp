af003a5cd73111cd1619e194755a1f40ac672ccd
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-18 15:30:51 -0600

    Ejercicio #4 de la guía terminado

---
+++ Ejercicio4.cpp
@@ -0,0 +1,20 @@
+//4. Escribir un programa que determine si un número ingresado por el usuario es primo.
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num;
+
+    cout << "Dime un numero y te dire si es primo: " << endl;
+    cin >> num;
+
+    if(num % num == 0){
+        cout << "El numero " << num << " es primo" << endl;
+    }else{
+        cout << "El numero " << num << " NO es primo" << endl;
+    }
+    return 0;
+}
+
