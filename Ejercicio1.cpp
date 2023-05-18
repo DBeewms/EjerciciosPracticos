4a7f5507ac1123ca39c43c9f8040f1bccdd21919
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-18 14:17:45 -0600

    Ejercicio #1 de la guía terminado

---
+++ Ejercicio1.cpp
@@ -0,0 +1,12 @@
+//1. Escribir un programa que muestre los números del 1 al 10 en orden ascendente.
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for(int i=1; i<11; i++){
+        cout << i << endl;
+    }
+    return 0;
+}
