9859c209f5b39930f144cb2a4523514b72b0ea34
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-18 19:34:15 -0600

    Encontré otro error lógico en Ejercicio4.cpp, se solucionó

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -3,21 +3,26 @@
 #include <iostream>
 using namespace std;
 
+
 int main(int argc, char const *argv[])
 {
-    int num;
-
-    cout << "Dime un numero y te dire si es primo: " << endl;
+    int num, contador = 0;
+	
+	cout << "Dime un numero: " << endl;
     cin >> num;
-
-    if(num % 2 == 0){
-        cout << "El numero " << num << " NO es primo" << endl;
-    }else{
-        if(num % num == 0){
-            cout << "El numero " << num << " es primo" << endl;
-        }
-    }
-    
-    return 0;
+	
+	for(int i=1; i <= num; i++){
+		if(num % i ==0){
+			contador++;
+		}
+	}
+	
+	if(contador > 2){
+		cout << "El numero " << num << " NO es primo" << endl;
+	}
+	else{
+		cout << "El numero " << num << " es primo" << endl;
+	}
+	
+	return 0;
 }
-
