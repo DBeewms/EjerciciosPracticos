311af86428dd3f93501f2943c969917c6b8af5c0
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-18 18:29:20 -0600

    Ejercicio4.cpp tenía un error lógico, solucionado

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -10,11 +10,14 @@ int main(int argc, char const *argv[])
     cout << "Dime un numero y te dire si es primo: " << endl;
     cin >> num;
 
-    if(num % num == 0){
-        cout << "El numero " << num << " es primo" << endl;
-    }else{
+    if(num % 2 == 0){
         cout << "El numero " << num << " NO es primo" << endl;
+    }else{
+        if(num % num == 0){
+            cout << "El numero " << num << " es primo" << endl;
+        }
     }
+    
     return 0;
 }
 
